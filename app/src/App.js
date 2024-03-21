57aab1959f0c1dba829b865c2954d18c4d4642d8
Dylan Wraith <dylanwraith@gmail.com>
2024-03-20 14:50:37 -1000

    OPR-2: Introduce homepage

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import top from './main-menu/top-border.png';
+import fun from './main-menu/menu-option-fun-mode.png';
+import resume from './main-menu/menu-option-resume.png';
+import projects from './main-menu/menu-option-projects.png';
+import about from './main-menu/menu-option-about-me.png';
+import contact from './main-menu/menu-option-contact-me.png';
+import bottom from './main-menu/bottom-border.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class={"container"} >
+        <div className={"menu-item"}>
+            <img src={top} alt="fun-mode" width="100%" />
+        </div>
+        <div className={"menu-item"}>
+            <img src={fun} alt="fun-mode" width="100%" />
+        </div>
+        <div className={"menu-item"}>
+            <img src={resume} alt="fun-mode" width="100%" />
+        </div>
+        <div className={"menu-item"}>
+            <img src={projects} alt="fun-mode" width="100%" />
+        </div>
+        <div className={"menu-item"}>
+            <img src={about} alt="fun-mode" width="100%" />
+        </div>
+        <div className={"menu-item"}>
+            <img src={contact} alt="fun-mode" width="100%" />
+        </div>
+        <div className={"menu-item"}>
+            <img src={bottom} alt="fun-mode" width="100%" />
+        </div>
     </div>
   );
 }
