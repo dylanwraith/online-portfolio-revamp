664ce4e3365ede0ae36bf2569bd46a49f1847c53
Dylan Wraith <dylanwraith@gmail.com>
2024-03-21 11:38:01 -1000

    OPR-8: Highlight menu-item on hover

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import top from './main-menu/top-border.png';
 import fun from './main-menu/menu-option-fun-mode.png';
@@ -7,30 +6,46 @@ import projects from './main-menu/menu-option-projects.png';
 import about from './main-menu/menu-option-about-me.png';
 import contact from './main-menu/menu-option-contact-me.png';
 import bottom from './main-menu/bottom-border.png';
+import selector from './main-menu/menu-selector.png';
 
 function App() {
   return (
-    <div class={"container"} >
+    <div class={"menu"} >
         <div className={"menu-item"}>
-            <img src={top} alt="fun-mode" width="100%" />
+            <img src={top} width="100%" />
         </div>
         <div className={"menu-item"}>
-            <img src={fun} alt="fun-mode" width="100%" />
+            <div className={"selectable"} >
+                <img src={fun} className={"fun-label"} />
+                <img src={selector} className={"fun-selector"} />
+            </div>
         </div>
         <div className={"menu-item"}>
-            <img src={resume} alt="fun-mode" width="100%" />
+            <div className={"selectable"} >
+                <img src={resume} className={"resume-label"} />
+                <img src={selector} className={"resume-selector"} />
+            </div>
         </div>
         <div className={"menu-item"}>
-            <img src={projects} alt="fun-mode" width="100%" />
+            <div className={"selectable"} >
+                <img src={projects} className={"projects-label"} />
+                <img src={selector} className={"projects-selector"} />
+            </div>
         </div>
         <div className={"menu-item"}>
-            <img src={about} alt="fun-mode" width="100%" />
+            <div className={"selectable"} >
+                <img src={about} className={"about-label"} />
+                <img src={selector} className={"about-selector"} />
+            </div>
         </div>
         <div className={"menu-item"}>
-            <img src={contact} alt="fun-mode" width="100%" />
+            <div className={"selectable"} >
+                <img src={contact} className="contact-label" />
+                <img src={selector} className={"contact-selector"} />
+            </div>
         </div>
         <div className={"menu-item"}>
-            <img src={bottom} alt="fun-mode" width="100%" />
+            <img src={bottom} width="100%" />
         </div>
     </div>
   );
