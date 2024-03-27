e282cd08133be0087f55ad71a45eb39e7f661c2e
Dylan Wraith <dylanwraith@gmail.com>
2024-03-26 14:34:25 -1000

    OPR-20: Refactor FE to use components

---
+++ app/src/pages/MainMenu/components/MainMenuButton.tsx
@@ -0,0 +1,57 @@
+import winky from '../assets/winky-pikachu.gif';
+import selector from '../assets/menu-selector.svg';
+import { FC } from 'react';
+import styled from 'styled-components';
+import NonSelectableImage from '../../../components/non-selectable-image';
+
+interface MainMenuButtonProps {
+	label: string;
+	isSelected: boolean;
+	isFunMode: boolean;
+	onMouseEnter: React.MouseEventHandler<HTMLLIElement>;
+}
+
+const Winky = styled(NonSelectableImage)({
+	position: 'absolute',
+	top: '5%',
+	height: '70%',
+	left: '77%',
+	cursor: 'pointer',
+});
+
+const Selector = styled(NonSelectableImage)({
+	position: 'absolute',
+	height: '40%',
+	left: '15px',
+	top: '30%',
+});
+
+const MainMenuButtonWrapper = styled.li({
+	position: 'relative',
+});
+
+const MainMenuButtonStyled = styled.button({
+	fontFamily: 'Pokemon Classic Regular',
+	width: '100%',
+	height: '100%',
+	backgroundColor: 'transparent',
+	borderWidth: '0',
+	textAlign: 'left',
+	cursor: 'pointer',
+	textIndent: '15%',
+	fontSize: 'x-large',
+});
+
+const MainMenuButton: FC<MainMenuButtonProps> = ({ label, isSelected, isFunMode, onMouseEnter }) => {
+	return (
+		<MainMenuButtonWrapper onMouseEnter={onMouseEnter}>
+			<MainMenuButtonStyled>
+				{label}
+				{isFunMode && <Winky src={winky} />}
+			</MainMenuButtonStyled>
+			{isSelected && <Selector src={selector} />}
+		</MainMenuButtonWrapper>
+	);
+};
+
+export default MainMenuButton;
